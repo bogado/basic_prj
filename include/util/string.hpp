a3abf3e3fe05f1c510410679b6540001b4a3f080
Victor Bogado da Silva Lins <victor@bogado.net>
2023-06-01 19:52:30 -0700

    fix the static string implementation

--- include/util/string.hpp
+++ include/util/string.hpp
@@ -9,58 +9,33 @@
 
 namespace vb {
 
+template <typename STRING_TYPE>
+concept is_string_type = std::same_as<std::char_traits<typename STRING_TYPE::value_type>, typename STRING_TYPE::traits_type> ||
+    std::is_array_v<STRING_TYPE>;
+
 inline namespace constexpr_str {
 
-// NOLINTBEGIN modernize-avoid-c-arrays
-template<size_t N>
-struct static_string {
-
-    consteval explicit static_string(const char (&str)[N]) // NOLINT(noExplicitConstructor)
-    : value{}
-    {
-        std::copy_n(str, N, std::begin(value));
-    }
-
-    constexpr operator std::string_view() const {
-        return std::string_view(value.data(), size());
-    }
-
-    constexpr auto size() const {
-        return N - 1*(value.back() == 0);
-    }
-
-    std::array<char, N> value;
-};
-
-template <char ... DATA>
-consteval auto operator""_str() {
-    return static_string<sizeof...(DATA)>(DATA...);
+consteval auto operator""_str(const char* str, std::size_t len)
+{
+    return std::string_view(str, len);
 }
-// NOLINTEND modernize-avoid-c-arrays //
 
+// NOLINTEND modernize-avoid-c-arrays //
 template <std::size_t N>
 using static_string_list = std::array<std::string_view, N>;
 
-template <static_string STR>
-static constexpr auto str_view = std::string_view(STR);
-
-template <static_string STR, auto SEPARATOR = ','>
-static constexpr auto split_count = std::ranges::count(str_view<STR>, SEPARATOR) + 1;
-
-template <static_string STR, auto SEPARATOR = ','>
-using splited_type = static_string_list<split_count<STR, SEPARATOR>>;  
-
-template <static_string STR, auto SEPARATOR = ','>
+template <is_string_type auto STR, auto SEPARATOR = ','>
 constexpr auto splited = []() {
-    constexpr auto view = str_view<STR>;
+    constexpr auto view =  std::string_view {STR};
+
+    static auto result = std::array<std::string_view, std::size(STR)>{};
+
     auto strings = view | std::views::split(SEPARATOR) | std::views::transform([](const auto& range) {
         return std::string_view(std::ranges::data(range), std::ranges::size(range));
     });
 
-    auto result = splited_type<STR, SEPARATOR>{};
-
-    std::ranges::copy(strings, std::begin(result));
-    return result;
+    auto end = std::ranges::copy(strings, std::begin(result));
+    return std::span{std::begin(STR), end};
 }();
 
 }
@@ -69,10 +44,10 @@ namespace test
 {
     using namespace std::literals;
 
-    static constexpr auto splited_test = "test,a,b,c"_str;
+    static constexpr auto splited_test = splited<"test,a,b,c"_str>;
     static constexpr auto first = splited_test[0];
 
-    static_assert(std::same_as<decltype(first), const std::string_view>);
+    static_assert(std::same_as<decltype(first), std::string_view>);
     static_assert(std::size(splited_test) == 4);
     static_assert(first == "test"sv);
     static_assert(splited_test[1] == "a"sv);
