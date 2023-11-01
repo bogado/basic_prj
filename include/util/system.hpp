2f18e601ab9ee26c606e279afd4590f3e3e12ba7
Victor Bogado da Silva Lins <vbogadodasil@bloomberg.net>
2023-11-01 13:46:45 -0400

    Adds propper debugging to the system calls.

--- include/util/system.hpp
+++ include/util/system.hpp
@@ -18,6 +18,8 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
+#include "debug.hpp"
+
 namespace vb {
 
 namespace fs = std::filesystem;
@@ -26,13 +28,16 @@ static constexpr auto KB = std::size_t{1024};
 namespace sys {
 
 static constexpr auto PAGE_SIZE = 4 * KB;
-template <typename... ARGS, typename INVOCABLE>
+template <typename... ARGS, typename INVOCABLE, std::size_t IGNORED_SIZE = 0>
 requires std::invocable<INVOCABLE, ARGS...>
-constexpr auto throw_on_error(INVOCABLE invocable) {
+constexpr auto throw_on_error(std::string_view name, INVOCABLE invocable, std::array<int, IGNORED_SIZE> ignored = {}) {
     return [=](ARGS... args, std::source_location source = std::source_location::current()) {
         auto error = [=](auto err) { 
             auto code = std::error_code(err, std::system_category());
             return std::system_error(code, 
+                std::string(name) +
+                " ERRNO = " + std::to_string(errno) +
+                " ⇒ " +
                 code.message() +
                 " at " +
                 source.function_name() +
@@ -40,9 +45,16 @@ constexpr auto throw_on_error(INVOCABLE invocable) {
                 source.file_name() + ":" + std::to_string(source.line()) + ":" + std::to_string(source.column()));
         };
 
+        debug(name);
         auto result = invocable(args...);
         using result_t = decltype(result);
+        debug("( "); (debug(args, " "), ...); debug.log_to(std::cerr, " ) = ", result);
 
+        if constexpr (IGNORED_SIZE != 0) {
+            if (std::find(std::begin(ignored), std::end(ignored), errno) != std::end(ignored)) {
+                return result;
+            }
+        }
         if constexpr (std::integral<result_t>)
         {
             if (result == -1) {
@@ -60,20 +72,22 @@ constexpr auto throw_on_error(INVOCABLE invocable) {
 template <std::size_t N_ARGS>
 auto exec(fs::path exe, std::array<std::string, N_ARGS> data, std::source_location source = std::source_location::current())
 {
-    constexpr auto exec = throw_on_error<const char*, char* const[]>(::execv);
+    constexpr auto exec = throw_on_error<const char*, char* const[]>("execv", ::execv);
     auto args_arr = std::array<char *, N_ARGS + 2>{};
     auto executable = exe.string();
     args_arr[0] = executable.data();
     std::ranges::copy(data | std::views::transform([](auto& dt) { return dt.data(); }), std::next(std::begin(args_arr)));
     args_arr.back() = nullptr;
-    for (auto a: args_arr)  { std::cerr << (a?a:"«nullptr»") << " «←\n"; }
+
+    debug.log_to(std::cerr, "exec( ", data, ") ");
+
     return exec(executable.c_str(), args_arr.data(), source);
 }
 
 auto pipe(std::source_location source = std::source_location::current())
 -> std::array<int,2>
 {
-    constexpr auto pipe = throw_on_error<>([]() {
+    constexpr auto pipe = throw_on_error<>("pipe", []() {
         int fds[2];
         ::pipe(fds);
         return std::array { fds[0], fds[1] };
@@ -81,30 +95,33 @@ auto pipe(std::source_location source = std::source_location::current())
     return pipe(source);
 }
 
+using status_type = std::optional<int>;
+
 auto wait_pid(pid_t pid, int option = 0, std::source_location source = std::source_location::current())
--> std::optional<int>
+-> status_type
 {
-    constexpr auto sys_waitpid = throw_on_error<pid_t, int*, int>(::waitpid);
+    constexpr auto sys_waitpid = throw_on_error<pid_t, int*, int>("waitpid", ::waitpid, std::array{ ECHILD });
     int status{0};
     sys_waitpid(pid, &status, option, source);
     if (status == 0) {
-        return std::optional<int>{};
+        return status_type{};
     }
     return WEXITSTATUS(status);
 }
 
 auto status_pid(pid_t pid, std::source_location source = std::source_location::current())
+-> status_type
 {
     return wait_pid(pid, WNOHANG, source);
 }
 
-constexpr auto dup2    = throw_on_error<int, int>(::dup2);
-constexpr auto fork    = throw_on_error<>(::fork);
-constexpr auto signal  = throw_on_error<int, void(*)(int)>(::signal);
-constexpr auto read    = throw_on_error<int, void*, std::size_t>(::read);
-constexpr auto write   = throw_on_error<int, const void*, std::size_t>(::write);
-constexpr auto close   = throw_on_error<int>(::close);
-constexpr auto open    = throw_on_error<const char*, int>(::open);
+constexpr auto dup2    = throw_on_error<int, int>("dup2", ::dup2);
+constexpr auto fork    = throw_on_error<>("fork", ::fork);
+constexpr auto signal  = throw_on_error<int, void(*)(int)>("signal", ::signal);
+constexpr auto read    = throw_on_error<int, void*, std::size_t>("Read", ::read);
+constexpr auto write   = throw_on_error<int, const void*, std::size_t>("write", ::write);
+constexpr auto close   = throw_on_error<int>("close", ::close);
+constexpr auto open    = throw_on_error<const char*, int>("open", ::open);
 
 }}
 
