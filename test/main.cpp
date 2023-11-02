65041ed0b8ba023919123f3646795268ba2827f1
Victor Bogado da Silva Lins <vbogadodasil@bloomberg.net>
2023-11-02 15:47:22 -0400

    Update main to run find in a controlled directory.
    Steps to turn this into a proper test latter.

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,13 @@
 #include <iterator>
 #include <string>
 #include <algorithm>
+#include <source_location>
+
+static constexpr auto test_dir = []() {
+    auto file = std::string_view{std::source_location::current().file_name()};
+    auto dir = file.substr(0, file.find_last_of('/'));
+    return dir;
+}();
 
 bool test_buffer()
 {
@@ -27,25 +34,10 @@ bool test_buffer()
         std::cout << hello.unload_line() << " ";
         if (!has_added) {
             hello.load(reader("end of the very large line\n"));
+            has_added = true;
         }
     }
     std::cout << "\n";
     return true;
 }
 
-using namespace std::literals;
-
-int main()
-{
-    static constexpr auto home = vb::env{"HOME"};
-    try {
-        test_buffer();
-        auto handler = vb::execution(vb::fs::path("/usr/bin/find"sv), std::array{home.value_or("."s), "-name"s, "*.hpp"s});
-        for (auto line : handler.stdout_lines()) {
-            std::cout << "→ " << line << "\n";
-        }
-        std::cout << "Exit: " <<  handler.wait() << "\n";
-    } catch (std::exception& e) {
-        std::cerr << "Caught exception : " << e.what() <<"\n";
-    }
-}
