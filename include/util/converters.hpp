a2a80e084d453ef128b0d6b39feb44ecd4f00e87
Victor Bogado da Silva Lins <vbogadodasil@bloomberg.net>
2023-11-14 16:41:12 -0500

    Update converters to be bidirectional

--- include/util/converters.hpp
+++ include/util/converters.hpp
@@ -10,20 +10,30 @@
 namespace vb::parse {
 
 template <typename VALUE_T>
-concept can_be_streamed = requires(std::istream& in, VALUE_T val) {
+concept can_be_istreamed = requires(std::istream& in, VALUE_T val) {
     { in >> val } -> std::same_as<std::istream&>;
 };
 
-template <typename PARSEABLE>
-auto from_string(std::string_view source) {
-    if constexpr (can_be_streamed<PARSEABLE>) {
-        auto result = PARSEABLE{};
-        std::stringstream in{std::string{source}};
-        in >> result;
-        return result;
-    } else {
-        throw "Not parseable";
-    }
+template <typename VALUE_T>
+concept can_be_outstreamed = requires(std::ostream& out, VALUE_T val) {
+    { out << val } -> std::same_as<std::ostream&>;
+};
+
+template <can_be_istreamed PARSEABLE>
+constexpr auto from_string(std::string_view source) {
+    auto result = PARSEABLE{};
+    std::stringstream in{std::string{source}};
+    in >> result;
+    return result;
+}
+
+template <can_be_outstreamed VALUE_T>
+constexpr auto to_string(const VALUE_T& value)
+-> std::string
+{
+    std::stringstream out{};
+    out << value;
+    return out.str();
 }
 
 template <typename PARSEABLE>
@@ -31,5 +41,12 @@ concept parseable = requires (const std::string_view str) {
     { from_string<PARSEABLE>(str) } -> std::same_as<PARSEABLE>;
 };
 
+template <typename STRINGABLE>
+concept stringable = requires (const STRINGABLE value)
+{
+    { to_string(value) } -> std::same_as<std::string>;
+};
+
 }
+
 #endif
