acd79bca1288f2dd27bd46bb94e819badfdfa273
Victor Bogado da Silva Lins <vbogadodasil@bloomberg.net>
2023-11-15 15:33:25 -0500

    Separate the tests into different spaces

--- test/execution.cpp
+++ test/execution.cpp
@@ -13,40 +13,12 @@ static constexpr auto test_dir = []() {
     return dir;
 }();
 
-bool test_buffer()
-{
-    auto reader = [](auto&& message) {
-        return [message](auto load, auto sz) {
-            auto end = std::copy_n(std::begin(message), std::min(std::size(message), sz), load);
-            return std::distance(load, end);
-        };
-    };
-
-    static constexpr std::size_t BIG = 1024u*5u;
-
-    auto hello = vb::buffer_type();
-    hello.load(reader("Hello\n"));
-    std::cout << hello.unload_line() << " ";
-    hello.load(reader("world\n!!\n"));
-    bool has_added = false;
-    hello.load(reader(std::string(BIG, '-')));
-    while(hello.has_data()) {
-        std::cout << hello.unload_line() << " ";
-        if (!has_added) {
-            std::cout << "\nloaded : " << hello.load(reader("end of the very large line\n")) << "\n";
-            has_added = true;
-        }
-    }
-    std::cout << "\n";
-    return true;
-}
 
 using namespace std::literals;
 
 int main()
 {
     try {
-        test_buffer();
         auto handler = vb::execution(vb::fs::path("/usr/bin/find"sv), std::array{std::string{test_dir}, "-print"s});
         for (auto line : handler.stdout_lines()) {
             std::cout << "→ " << line << "\n";
