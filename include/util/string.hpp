e524e46c83f59b34c8af49292eafd5bde4a42a0b
Victor Bogado da Silva Lins <vbogadodasil@bloomberg.net>
2024-09-13 12:46:18 -0400

    Add more operations to the static string.

--- include/util/string.hpp
+++ include/util/string.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <concepts>
+#include <cstddef>
 #include <limits>
 #include <string>
 #include <string_view>
@@ -18,16 +19,17 @@ concept is_string_type = std::same_as<std::char_traits<typename STRING_TYPE::val
 
 // NOLINTBEGIN modernize-avoid-c-arrays
 template <std::size_t LENGTH, typename VALUE_T = char, typename TRAITS = std::char_traits<VALUE_T>>
-requires (LENGTH > 0)
 struct static_string {
     using storage_type     = std::array<VALUE_T, LENGTH>;
 	using traits_type      = TRAITS;
 	using value_type       = VALUE_T;
     using char_type        = VALUE_T;
     using size_type        = std::size_t;
+    using difference_type  = std::ptrdiff_t;
 	using string_type      = std::basic_string<value_type, traits_type>;
 	using string_view_type = std::basic_string_view<value_type, traits_type>;
 	using value_limits     = std::numeric_limits<value_type>;
+    using iterator         = storage_type::const_iterator;
 
     static constexpr auto length = LENGTH;
 
@@ -35,34 +37,99 @@ struct static_string {
 
     // No escape from c arrays here
     // NOLINTNEXTLINE(cppcoreguidelines-pro-type-member-init,cppcoreguidelines-avoid-c-arrays) 
-	constexpr static_string(const value_type (&s)[LENGTH])
+	constexpr static_string(const value_type (&s)[LENGTH]) noexcept
     {
         std::copy_n(std::begin(s), LENGTH, std::begin(content));
     }
 
-	constexpr auto view() const 
+	constexpr auto view() const noexcept
         -> string_view_type
     {
-		return string_view_type{content.data(), LENGTH};
+		return string_view_type{content.data(), size()};
 	}
 
-	constexpr auto size() const { return LENGTH - (*std::rbegin(content) == '\0'? 1: 0); }
+    constexpr auto front() const noexcept {
+        if constexpr (length == 0) {
+            return '\0';
+        }
+        return content.front();
+    }
 
-    constexpr auto operator<=>(const static_string& other) const
+    constexpr auto back() const noexcept {
+        if constexpr (length == 0) {
+            return '\0';
+        }
+        return content.back();
+    }
+
+    constexpr auto substr(size_type start, difference_type len = length) const noexcept {
+        if (start >= size()) {
+            return std::string_view{};
+        }
+
+        auto start_loc = std::next(std::begin(content), static_cast<difference_type>(start));
+        return std::string_view{start_loc, std::next(start_loc, std::min<difference_type>(ssize() - static_cast<difference_type>(start), len))};
+    }
+
+    constexpr auto operator[](size_t pos) const noexcept
+    {
+        if (pos >= size()) {
+            return '\0';
+        }
+        return content[pos];
+    }
+
+    /// The last non '\0' element.
+    constexpr auto last() const noexcept {
+        auto tail = content | std::views::reverse | std::views::drop_while([](auto chr) { return chr == '\0'; }) | std::views::take(1);
+        return tail.front();
+    }
+
+	constexpr auto size() const noexcept { return LENGTH - (*std::rbegin(content) == '\0'? 1: 0); }
+	constexpr auto ssize() const noexcept { return static_cast<difference_type>(size()); }
+
+    constexpr auto begin() const noexcept { return std::begin(content); }
+    constexpr auto end() const noexcept { return std::end(content); }
+
+    template <size_type OTHER_SIZE>
+    constexpr auto operator<=>(const static_string<OTHER_SIZE>& other) const noexcept
     {
         return view() <=> other.view();
     }
 
-    constexpr auto array() const { return content; }
+    constexpr auto array() const noexcept { return content; }
 
-    constexpr bool operator==(const static_string& other) const = default;
-    constexpr bool operator!=(const static_string& other) const = default;
+    template <size_type OTHER_SIZE>
+    requires(OTHER_SIZE != LENGTH)
+    constexpr bool operator==(const static_string<OTHER_SIZE>&) const noexcept { return false; }
+
+    template <size_type OTHER_SIZE>
+    requires(OTHER_SIZE != LENGTH)
+    constexpr bool operator!=(const static_string<OTHER_SIZE>&) const noexcept { return true; }
+
+    constexpr bool operator==(const static_string& other) const noexcept = default;
+    constexpr bool operator!=(const static_string& other) const noexcept = default;
 };
 
 namespace test {
 
 static constexpr auto test = static_string("123");
-static_assert(test.size() == 3); // The final '\0' is included.
+static constexpr auto test2 = static_string("234");
+static constexpr auto test3 = static_string("2345");
+static_assert(test.size() == 3);
+static_assert(test.substr(1,1) == "2");
+static_assert(test.back() == '\0');
+static_assert(test.last() == '3');
+static_assert(test >= test);
+static_assert(test == "123");
+static_assert(test != test2);
+static_assert(test < test3);
+static_assert(test3 > test);
+static_assert(test < test2);
+static_assert(test2 > test);
+static_assert(test.view() != test2.substr(1));
+static_assert(test3.substr(0,3) == "234");
+static_assert(test2.view() == test3.substr(0, 3));
 
 }  // namespace literals
 
