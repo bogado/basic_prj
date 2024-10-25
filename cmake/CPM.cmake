fc145c3fca44d18e0525400cb0fc0769d3fbc4e6
Victor Bogado da Silva Lins <vbogadodasil@bloomberg.net>
2024-10-25 21:26:18 +0000

    Update CPM version and check the result.

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -1,4 +1,4 @@
-set(CPM_DOWNLOAD_VERSION 0.38.1)
+set(CPM_DOWNLOAD_VERSION 0.40.2)
 
 if(CPM_SOURCE_CACHE)
   set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
@@ -13,10 +13,15 @@ get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
 function(download_cpm)
   message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
-  file(DOWNLOAD
+  file(DOWNLOAD 
        https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
        ${CPM_DOWNLOAD_LOCATION}
+       STATUS CPM_DOWNLOAD_STATUS
   )
+  list(POP_BACK CPM_DOWNLOAD_STATUS CPM_ERROR)
+  if (NOT ${CPM_DOWNLOAD_STATUS} EQUAL 0)
+      message(FATAL_ERROR "CPM could not be downloaded ${CPM_ERROR} : ${CPM_DOWNLOAD_STATUS}")
+  endif()
 endfunction()
 
 if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
