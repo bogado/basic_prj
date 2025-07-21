84e287e0c75b29f2e92f7b468e4bcd0d496c07d8
Victor Bogado da Silva Lins <victor@bogado.net>
2025-07-21 13:25:34 -0400

    Make the sanitezers and other options useable with more targets.
    INTERFACE targets were not working correctly.

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,14 +1,14 @@
 include(${CMAKE_CURRENT_LIST_DIR}/Utilities.cmake)
 
-function(enable_sanitizers project_name)
+function(enable_sanitizers project_name type)
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES
                                              ".*Clang")
     option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
 
     if(ENABLE_COVERAGE)
-      target_compile_options(${project_name} INTERFACE --coverage -O0 -g -fprofile-arcs -ftest-coverage)
-      target_link_libraries(${project_name} INTERFACE --coverage)
+      target_compile_options(${project_name} ${type} --coverage -O0 -g -fprofile-arcs -ftest-coverage)
+      target_link_libraries(${project_name} ${type} --coverage)
     endif()
 
     set(SANITIZERS "")
@@ -40,8 +40,8 @@ function(enable_sanitizers project_name)
 
   if(LIST_OF_SANITIZERS)
     if(NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
-        target_compile_options(${project_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
-        target_link_options(${project_name} PRIVATE -fsanitize=${LIST_OF_SANITIZERS})
+        target_compile_options(${project_name} ${type} -fsanitize=${LIST_OF_SANITIZERS})
+        target_link_options(${project_name} ${type} -fsanitize=${LIST_OF_SANITIZERS})
     endif()
   endif()
 
--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -12,22 +12,20 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
                                                "MinSizeRel" "RelWithDebInfo")
 endif()
 
-function(enable_clang_features target)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        option(USE_LIBCXX_STDLIB "Use libc++ instead of libstdc++" on)
-        if (USE_LIBCXX_STDLIB)
-            add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
-            target_compile_options(basic_options INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
-            add_link_options(-stdlib=libc++)
-            target_link_options(${target} INTERFACE -stdlib=libc++)
-        endif()
-
-        option(ENABLE_BUILD_WITH_TIME_TRACE "Enable -ftime-trace to generate time tracing .json files on clang" off)
-        if (ENABLE_BUILD_WITH_TIME_TRACE)
-            target_compile_options(${target} INTERFACE -ftime-trace)
-        endif()
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    option(USE_LIBCXX_STDLIB "Use libc++ instead of libstdc++" on)
+    if (USE_LIBCXX_STDLIB)
+        add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+        target_compile_options(basic_options INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+        add_link_options(-stdlib=libc++)
+        target_link_options(${target} INTERFACE -stdlib=libc++)
     endif()
-endfunction()
+
+    option(ENABLE_BUILD_WITH_TIME_TRACE "Enable -ftime-trace to generate time tracing .json files on clang" off)
+    if (ENABLE_BUILD_WITH_TIME_TRACE)
+        target_compile_options(${target} INTERFACE -ftime-trace)
+    endif()
+endif()
 
 # Generate compile_commands.json to make it easier to work with clang based
 # tools
@@ -48,10 +46,8 @@ if(ENABLE_IPO)
 endif()
 
 function(setup_target target type)
-    if(NOT type STREQUAL "INTERFACE")
-        set_project_warnings(${target} ${type})
-        enable_sanitizers(${target})
-    endif()
+    set_project_warnings(${target} ${type})
+    enable_sanitizers(${target} ${type})
     set_target_properties(${target} PROPERTIES
         CMAKE_CXX_STANDARD 23
         CMAKE_CXX_EXTENSIONS False)
