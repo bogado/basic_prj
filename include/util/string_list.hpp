ce01d11f6c63b18f6998f7e856bfb881d0045e81
Victor Bogado da Silva Lins <victor@bogado.net>
2025-08-12 19:29:46 -0400

    Adding tools to help split strings.
    Including the capability of partially splitting a string.

--- include/util/string_list.hpp
+++ include/util/string_list.hpp
@@ -4,37 +4,75 @@
 #include "./string.hpp"
 
 #include <algorithm>
+#include <array>
+#include <format>
 #include <iterator>
+#include <limits>
 #include <ranges>
+#include <stdexcept>
+#include <string_view>
+#include <type_traits>
+#include <vector>
 
 namespace vb {
 
 using namespace std::literals;
 
-template<std::size_t N>
-using static_string_list = std::array<std::string_view, N>;
+static constexpr inline auto NO_MAXIMUM             = std::numeric_limits<std::size_t>::max();
+static constexpr inline auto DEFAULT_LIST_SEPARATOR = ',';
 
-template<static_string STR, auto SEPARATOR = ',', std::size_t max = 0>
-constexpr auto splited = []() {
-    constexpr auto view = as_string_view(STR);
+constexpr auto count_parts(
+    const is_string auto& source,
+    char                  separator = DEFAULT_LIST_SEPARATOR,
+    std::size_t           max_count = NO_MAXIMUM) -> std::size_t
+{
+    return std::min(static_cast<std::size_t>(std::ranges::count(as_string_view(source), separator) + 1), max_count);
+}
 
-    auto result = [&]() {
-        if constexpr (max == 0) {
-            // "1,2,3" :
-            //   - 2 separators
-            //   - 3 items.
-            return std::array<std::string_view, std::ranges::count(view, SEPARATOR) + 1>{};
+template<std::size_t MAX = NO_MAXIMUM>
+constexpr auto split_string(const is_string auto& source, char separator = DEFAULT_LIST_SEPARATOR)
+{
+    auto       view   = as_string_view(source);
+    const auto count  = count_parts(source, separator, MAX);
+    auto       result = [&]() {
+        if constexpr (MAX == NO_MAXIMUM) {
+            return std::vector<std::string_view>{ count, std::string_view{} };
         } else {
-            return std::array<std::string_view, max>{};
+            return std::array<std::string_view, MAX>{};
         }
     }();
 
-    auto strings = view | std::views::split(SEPARATOR) | std::views::transform([](const auto& range) {
-                       return std::string_view(std::ranges::data(range), std::ranges::size(range));
-                   });
+    for (auto [index, ret] : std::ranges::iota_view(std::size_t{ 0 }, count) |
+                                 std::views::transform([&, it = std::begin(view)](const auto& index) mutable {
+                                     if (index == count - 1) {
+                                         return std::pair{ index, std::string_view(it, std::end(view)) };
+                                     }
 
-    std::ranges::copy(strings | std::views::take(result.size()), std::begin(result));
+                                     auto start = it;
+                                     auto end   = std::ranges::find(start, std::end(view), separator);
+                                     if (end != std::end(view)) {
+                                         it = std::next(end, 1);
+                                     } else {
+                                         it = end;
+                                     }
+                                     return std::pair{ index, std::string_view(start, end) };
+                                 })) {
+        result.at(index) = ret;
+    }
     return result;
+}
+
+template<std::size_t N>
+using static_string_list = std::array<std::string_view, N>;
+
+template<static_string STR, auto SEPARATOR = ',', std::size_t MAX = NO_MAXIMUM>
+constexpr auto splited = []() {
+    constexpr auto view  = as_string_view(STR);
+    constexpr auto count = count_parts(view, SEPARATOR, MAX);
+    static_assert(count != NO_MAXIMUM);
+    static_assert(count > 0);
+
+    return split_string<count>(view, SEPARATOR);
 }();
 
 namespace test {
@@ -49,11 +87,18 @@ static_assert(splited_test[1] == "a"sv);
 static_assert(splited_test[2] == "b"sv);
 static_assert(splited_test[3] == "c"sv);
 
+static_assert(count_parts("1,2,3,4") == 4);
+static_assert(count_parts("1.2.3.4") == 1);
+static_assert(count_parts("1.2.3.4", '.') == 4);
+static_assert(count_parts("1.2.3.4", '.', 2) == 2);
+static_assert(count_parts("1.2.3.4", ',', 2) == 1);
+
 static constexpr auto splited_test_2 = splited<"test.1.2.3", '.', 2>;
 static_assert(splited_test_2.size() == 2);
 static_assert(splited_test_2[0] == "test");
 static_assert(splited_test_2[1] == "1.2.3");
 } // namespace test
+
 }
 
 #endif
